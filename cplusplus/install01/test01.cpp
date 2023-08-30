de9802e5abe310fabefcfc8f831d6ad32d6b96e7
dawwsoo <dmstnjo@gmail.com>
2023-08-30 15:37:20 +0900

    0830 수업 실습 업데이트

---
+++ cplusplus/install01/test01.cpp
@@ -0,0 +1,10 @@
+//#include <iostream>
+//int main() {
+//	int num=0;
+//	std::cout << "숫자를 입력하세요 : ";
+//	std::cin >> num;
+//
+//	num % 5 == 0 ? std::cout << num << "은(는) 5의 배수입니다": std::cout << num << "은(는) 5의 배수가 아니네요......";
+//	
+//
+//}
