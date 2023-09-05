a2cbeec2f3ec3f3ad9b726162a276d9002807086
dawwsoo <dmstnjo@gmail.com>
2023-09-05 16:02:19 +0900

    0905 실습

---
+++ lec_first/install01/test02.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//int main() {
+//	int num1 = 0;
+//	int num2 = 0;
+//	std::string cal = "";
+//
+//	std::cout << " 두 수를 입력하세요. (숫자 한 번 입력 시 스페이스) \n";
+//	std::cin >> num1 >> num2;
+//
+//	std::cout << " 연산자를 입력하세요. \n";
+//	std::cin >> cal;
+//
+//	if (cal == "+") {
+//		std::cout << num1 << " + " << num2 << " = " << num1 + num2 << "\n";
+//	}
+//	if (cal == "-") {
+//		std::cout << num1 << " - " << num2 << " = " << num1 - num2 << "\n";
+//
+//	}
+//	if (cal == "/") {
+//		std::cout << num1 << " / " << num2 << " = " << num1 / num2 << "..." << num1 % num2 << "\n";
+//
+//	}
+//	if (cal == "*") {
+//		std::cout << num1 << " * " << num2 << " = " << num1 * num2 << "\n";
+//
+//	}
+//}
